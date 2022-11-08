48dccda857e67cfe1617ce8ddceb0753b3fb0e39
Filiz Sav <filizsav@Filizs-MBP-2.local.tld>
2022-11-07 20:26:27 -0600

    Changes

--- src/string_methods/_13_replace_Method.java
+++ src/string_methods/_13_replace_Method.java
@@ -1,5 +1,7 @@
 package string_methods;
 
+import java.util.Arrays;
+
 public class _13_replace_Method {
     public static void main(String[] args) {
         /*
@@ -11,6 +13,7 @@ public class _13_replace_Method {
 
         String s = "Can I can a can";
 
+
         String s1 = s.replace('a','A');
         System.out.println(s1);
         System.out.println(s.toLowerCase().replace("can", "xxx"));
@@ -19,6 +22,12 @@ public class _13_replace_Method {
         str1 = str1.replace("o", "oooo");
         System.out.println(str1);
 
+        System.out.println(s.length() - s.replaceAll(" ", "").length() + 1);
+
+        String words = "I love Java so much";
+        System.out.println(words.length() - words.replaceAll("o", "").length());
+
+        System.out.println(words.subSequence(3,7));
     }
 
 }
